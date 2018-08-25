b709fc047ea981157065826777d76e1e14d6eeac
Oliver-Gomes <ogomesphoto@gmail.com>
2018-08-25 02:06:34 -0400

    even odd selector

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 // $('h1#heading1').hide();
 $('p span').css('color', 'red');
-$('ul#list li:first').css('color', 'red');
-$('ul#list li:last').css('color', 'green');
+$('ul#list li:first').css('color', 'BLUEVIOLET');
+$('ul#list li:last').css('color', 'DEEPPINK');
+$('ul#list li:even').css('background-color', 'MEDIUMAQUAMARINE');
+$('ul#list li:odd').css('background-color', 'BISQUE');
