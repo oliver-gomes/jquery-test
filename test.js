af04dfdb319092e81030571d85a669c84765b7d7
Oliver-Gomes <ogomesphoto@gmail.com>
2018-08-25 02:00:16 -0400

    practice with selectors

--- test.js
+++ test.js
@@ -1,7 +1,4 @@
-//var myPara = document.getElementById("content").getElementsByTagName("p")[5];
-
-//using jQuery doing the same thing
-var myPara2 = $("#content p:last-child");
-
-//myPara2.animate({left: "50px"});
-myPara2.css({color: "red"});
+// $('h1#heading1').hide();
+$('p span').css('color', 'red');
+$('ul#list li:first').css('color', 'red');
+$('ul#list li:last').css('color', 'green');
