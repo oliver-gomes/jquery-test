0e1100da899c82f0b83dc06be418cf816fcb9972
Oliver-Gomes <ogomesphoto@gmail.com>
2018-08-25 22:18:31 -0400

    button selectors and further

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
+//Selectors
+
 // $('h1#heading1').hide();
 $('p span').css('color', 'red');
 $('ul#list li:first').css('color', 'BLUEVIOLET');
 $('ul#list li:last').css('color', 'DEEPPINK');
 $('ul#list li:even').css('background-color', 'MEDIUMAQUAMARINE');
 $('ul#list li:odd').css('background-color', 'BISQUE');
+$('ul#list li:nth-child(3n').css('list-style', 'none');
+$(':text').hide();
+$('[href]').css('color', 'red')
+$('[href="http://yahoo.com"]').css('color', 'green');
+// $('*').hide();
